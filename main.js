d8511b4e7cc103f3168143a5e486fbc45b67c600
dream-well <dreamwell506@outlook.com>
2022-11-09 23:04:26 +0800

    allow

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require("morgan");
 const { createProxyMiddleware } = require('http-proxy-middleware');
+var AccessControl = require('express-ip-access-control');
+
 require('dotenv').config()
 
 // Creating express server
@@ -8,6 +10,21 @@ const app = express();
 
 const PORT = 8545;
 const RPC_URL = 'http://localhost:8546';
+const ALLOW = '185.246.188.105';
+
+app.use(AccessControl({
+    mode: 'deny',
+	denys: [],
+	allows: [ALLOW],
+	forceConnectionAddress: false,
+	log: function(clientIp, access) {
+		console.log(clientIp + (access ? ' accessed.' : ' denied.'));
+	},
+
+	statusCode: 401,
+	redirectTo: '',
+	message: 'Unauthorized'
+}))
 
 app.use(morgan("combined"));
 
